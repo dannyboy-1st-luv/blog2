837e96508f655aba79e68bdceaaa61654098f1ff
dannydarku <dannydarku@gmail.com>
2019-03-29 02:30:14 -0400

    updated for heroku

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  root 'pages#index'
+  get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users, except: [:new]
+
+  resources :articles
+
+end
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
